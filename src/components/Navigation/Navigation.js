0a888029260ba21724368ee9e24627e27cecfa2f
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-07-13 16:32:03 +0300

    attempt change style active link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,25 @@
 import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.css';
 
-const toogleActive = active => {
-  const { isActive } = active;
-  return isActive ? s.active : s.unactive;
-};
+// const toogleActive = active => {
+//   const { isActive } = active;
+//   return isActive ? s.active : s.unactive;
+// };
 
 const Navigation = () => {
   return (
     <nav className={s.nav}>
-      <NavLink to="/" className={toogleActive} exact="true">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? s.active : s.unactive)}
+        exact="true"
+      >
         Homepage
       </NavLink>
-      <NavLink to="/movies" className={toogleActive}>
+      <NavLink
+        to="/movies"
+        className={({ isActive }) => (isActive ? s.active : s.unactive)}
+      >
         Movies
       </NavLink>
     </nav>
